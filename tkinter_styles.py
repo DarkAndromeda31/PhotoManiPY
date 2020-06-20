ac4292aeecf0202d86f46d577ada1a1b52fe1054
DarkAndromeda31 <darkandromeda31@hotmail.com>
2020-06-20 19:57:15 +1000

    added tkinter_styles.py and sample resized image

---
+++ tkinter_styles.py
@@ -0,0 +1,8 @@
+"""
+Module: tkinter_styles for Photo ManiPY
+Description: A file that contains the styling for the tkinter elements
+
+Made by: Maximilian Rose
+Created on 20/6/2020
+IDE: PyCharm
+"""
