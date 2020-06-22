ec7dc7f8dcffeff1238255f0b169e56e031ed395
DarkAndromeda31 <octopi314159@gmail.com>
2020-06-22 20:08:17 +1000

    Converted entire program to ttk widgets Changed tkinter_styles.py a bit, still is placeholder for now

--- PhotoManiPY.py
+++ PhotoManiPY.py
@@ -16,6 +16,7 @@ from PIL import ImageTk
 import codec
 import edit_image as ei
 import manage_image as mi
+from tkinter_styles import *
 
 
 class Window(tk.Frame):
@@ -37,7 +38,7 @@ class Window(tk.Frame):
             crop = f"({tool_crop_top.get()},{tool_crop_bottom.get()})"
             resize = f"(x{tool_resize_vertical.get()},x{tool_resize_horizontal.get()})"
 
-            outpath = path.replace(pl.Path(path).suffix, "") + hsl + crop + resize + current_dropdown.get()
+            outpath = path.replace(pl.Path(path).suffix, "") + hsl + crop + resize + str(file_io_output_ext_dropdown.current())
 
             file_io_output_path.delete(0, tk.END)
             file_io_output_path.insert(0, outpath)
@@ -69,8 +70,8 @@ class Window(tk.Frame):
 
         def browse_file_out():
             """
-            Function that opens a folder browse dialouge then changes output path accordingly
-            TODO: Make this a functining system not whatever it is now
+            Function that opens a folder browse dialogue then changes output path accordingly
+            TODO: Make this a functioning system not whatever it is now
             """
             file_io_output_path.delete(0, tk.END)
             path = filedialog.askopenfilename(
@@ -140,7 +141,7 @@ class Window(tk.Frame):
 
             pic = ImageTk.PhotoImage(cur_image)
 
-            img = tk.Label(prev_win, image=pic)
+            img = ttk.Label(prev_win, image=pic)
             img.image = pic
 
             img.grid(row=0, column=0)
@@ -155,7 +156,7 @@ class Window(tk.Frame):
 
             # ----- FRAMES -----
             # Define the Frames for each section
-            toolbar = tk.Frame(gui_win, bd=1, relief="raised")
+            toolbar = ttk.Frame(gui_win, relief="raised")
             toolbar.grid(row=0, column=0)
 
             # ----- WIDGETS -----
@@ -163,7 +164,7 @@ class Window(tk.Frame):
             crop_icon = mi.open_image("./images/crop_icon.png")
             crop_image = ImageTk.PhotoImage(crop_icon)
 
-            gui_tool_crop_button = tk.Button(toolbar, image=crop_image)
+            gui_tool_crop_button = ttk.Button(toolbar, image=crop_image)
             gui_tool_crop_button.image = crop_image
 
             gui_tool_crop_button.grid(row=0, column=0)
@@ -173,7 +174,7 @@ class Window(tk.Frame):
 
             main_pic = ImageTk.PhotoImage(cur_image)
 
-            img = tk.Label(gui_win, image=main_pic)
+            img = ttk.Label(gui_win, image=main_pic)
             img.image = main_pic
 
             img.grid(row=1, column=0)
@@ -186,26 +187,26 @@ class Window(tk.Frame):
 
         # ----- FRAMES -----
         # Define the Frames for each section
-        file_io = tk.LabelFrame(self, text='File I/O')  # remove dimensions when filled
-        tool = tk.LabelFrame(self, text='Tools')
-        image = tk.LabelFrame(self, text='Image')
+        file_io = ttk.LabelFrame(self, text='File I/O')  # remove dimensions when filled
+        tool = ttk.LabelFrame(self, text='Tools')
+        image = ttk.LabelFrame(self, text='Image')
 
         file_io.grid(row=0, column=0, columnspan=2, sticky="NSEW", padx=5, pady=5)
         tool.grid(row=1, column=0, columnspan=3, rowspan=6, sticky="NSEW", padx=5, pady=5)
         image.grid(row=0, column=2, columnspan=2, sticky="NSEW", padx=5, pady=5)
 
         # Define sub-frames for file IO
-        file_io_input_ext = tk.LabelFrame(file_io, text='Input file extensions')
-        file_io_output_ext = tk.LabelFrame(file_io, text='Output extension')
+        file_io_input_ext = ttk.LabelFrame(file_io, text='Input file extensions')
+        file_io_output_ext = ttk.LabelFrame(file_io, text='Output extension')
 
         file_io_input_ext.grid(row=2, column=0, columnspan=2, sticky="NSEW", padx=5, pady=5)
         file_io_input_ext.grid_columnconfigure(0, weight=1)
         file_io_output_ext.grid(row=2, column=2, columnspan=1, sticky="NSW", padx=5, pady=5)
 
         # Define sub-frames for tools
-        tool_hue_shift = tk.LabelFrame(tool, text='Shift Hue')
-        tool_crop = tk.LabelFrame(tool, text='Crop')
-        tool_resize = tk.LabelFrame(tool, text='Resize')
+        tool_hue_shift = ttk.LabelFrame(tool, text='Shift Hue')
+        tool_crop = ttk.LabelFrame(tool, text='Crop')
+        tool_resize = ttk.LabelFrame(tool, text='Resize')
 
         tool_hue_shift.grid(row=0, column=0, rowspan=2, sticky="NSEW", padx=5, pady=5)
         tool_crop.grid(row=0, column=1, rowspan=2, sticky="NSEW", padx=5, pady=5)
@@ -213,12 +214,12 @@ class Window(tk.Frame):
 
         # ----- WIDGETS -----
         # Define widgets for file IO main
-        file_io_input_path_text = tk.Label(file_io, text="Input Path:\n(File or Folder)")
-        file_io_input_path = tk.Entry(file_io, width=75)
-        file_io_browse_in = tk.Button(file_io, text='Browse', command=browse_file_in)
-        file_io_output_path_text = tk.Label(file_io, text="Output Path:")
-        file_io_output_path = tk.Entry(file_io, width=75)
-        file_io_browse_out = tk.Button(file_io, text='Browse', command=browse_file_out)
+        file_io_input_path_text = ttk.Label(file_io, text="Input Path:\n(File or Folder)")
+        file_io_input_path = ttk.Entry(file_io, width=75)
+        file_io_browse_in = ttk.Button(file_io, text='Browse', command=browse_file_in)
+        file_io_output_path_text = ttk.Label(file_io, text="Output Path:")
+        file_io_output_path = ttk.Entry(file_io, width=75)
+        file_io_browse_out = ttk.Button(file_io, text='Browse', command=browse_file_out)
 
         file_io_input_path_text.grid(row=0, column=0, padx=5, pady=5)
         file_io_input_path.grid(row=0, column=1, columnspan=3, padx=5, pady=5)
@@ -228,40 +229,40 @@ class Window(tk.Frame):
         file_io_browse_out.grid(row=1, column=4, padx=5, pady=5)
 
         # Define widgets for file IO input extensions
-        file_io_input_ext_entry = tk.Entry(file_io_input_ext)
+        file_io_input_ext_entry = ttk.Entry(file_io_input_ext)
         file_io_input_ext_entry.insert(0, "*.png:*.jpg:*.jpeg")
 
         file_io_input_ext_entry.grid(row=0, column=0, sticky="NSEW", padx=5, pady=5)
 
         # Define widgets for file IO output extension
         dropdown_options = [".png", ".jpg", ".bmp", ".maxpg"]
-        current_dropdown = tk.StringVar(self)
-        current_dropdown.set(dropdown_options[0])
 
-        file_io_output_ext_dropdown = tk.OptionMenu(file_io_output_ext, current_dropdown, *dropdown_options)
+        file_io_output_ext_dropdown = ttk.Combobox(file_io_output_ext, values=dropdown_options)
+        file_io_output_ext_dropdown.current(0)
+
         file_io_output_ext_dropdown.grid(row=0, column=0, sticky="NSEW", padx=5, pady=5)
 
         # Define widgets for image preview
-        image_preview_test = tk.Label(image, text="Preview")
-        file_io_preview_open = tk.Button(image, text="Open", command=open_preview)
+        image_preview_test = ttk.Label(image, text="Preview")
+        file_io_preview_open = ttk.Button(image, text="Open", command=open_preview)
 
         image_preview_test.grid(row=0, column=0, padx=5, pady=5)
         file_io_preview_open.grid(row=0, column=1, padx=5, pady=5)
 
         # Define widgets for image GUI button
-        image_gui_test = tk.Label(image, text="Graphical Editor")
-        image_gui_open = tk.Button(image, text="Open", command=open_GUI)
+        image_gui_test = ttk.Label(image, text="Graphical Editor")
+        image_gui_open = ttk.Button(image, text="Open", command=open_GUI)
 
         image_gui_test.grid(row=1, column=0, padx=5, pady=5)
         image_gui_open.grid(row=1, column=1, padx=5, pady=5)
 
         # Define widgets for hue_shift
-        tool_hue_shift_hue_text = tk.Label(tool_hue_shift, text="Hue:")
-        tool_hue_shift_hue = tk.Spinbox(tool_hue_shift, from_=0, to=360, justify='right')
-        tool_hue_shift_sat_text = tk.Label(tool_hue_shift, text="Saturation:")
-        tool_hue_shift_sat = tk.Spinbox(tool_hue_shift, from_=-100, to=100, justify='right')
-        tool_hue_shift_lum_text = tk.Label(tool_hue_shift, text="Luminance:")
-        tool_hue_shift_lum = tk.Spinbox(tool_hue_shift, from_=-100, to=100, justify='right')
+        tool_hue_shift_hue_text = ttk.Label(tool_hue_shift, text="Hue:")
+        tool_hue_shift_hue = ttk.Spinbox(tool_hue_shift, from_=0, to=360, style="TSpinbox")
+        tool_hue_shift_sat_text = ttk.Label(tool_hue_shift, text="Saturation:")
+        tool_hue_shift_sat = ttk.Spinbox(tool_hue_shift, from_=-100, to=100, style="TSpinbox")
+        tool_hue_shift_lum_text = ttk.Label(tool_hue_shift, text="Luminance:")
+        tool_hue_shift_lum = ttk.Spinbox(tool_hue_shift, from_=-100, to=100, style="TSpinbox")
 
         tool_hue_shift_hue_text.grid(row=0, column=0, padx=5, pady=3)
         tool_hue_shift_hue.grid(row=0, column=1, padx=5, pady=3)
@@ -270,20 +271,22 @@ class Window(tk.Frame):
         tool_hue_shift_lum_text.grid(row=2, column=0, padx=5, pady=3)
         tool_hue_shift_lum.grid(row=2, column=1, padx=5, pady=3)
 
+        tool_hue_shift_hue.delete(0, tk.END)
+        tool_hue_shift_hue.insert(0, "0")
         tool_hue_shift_sat.delete(0, tk.END)
         tool_hue_shift_sat.insert(0, "0")
         tool_hue_shift_lum.delete(0, tk.END)
         tool_hue_shift_lum.insert(0, "0")
 
         # Define widgets for crop
-        tool_crop_top_text = tk.Label(tool_crop, text="Point 1 x:")
-        tool_crop_top = tk.Spinbox(tool_crop, from_=0, to=9999999999999, justify='right')
-        tool_crop_bottom_text = tk.Label(tool_crop, text="Point 2 x:")
-        tool_crop_bottom = tk.Spinbox(tool_crop, from_=0, to=9999999999999, justify='right')
-        tool_crop_top_text2 = tk.Label(tool_crop, text="Point 1 y:")
-        tool_crop_top2 = tk.Spinbox(tool_crop, from_=0, to=9999999999999, justify='right')
-        tool_crop_bottom_text2 = tk.Label(tool_crop, text="Point 2 y:")
-        tool_crop_bottom2 = tk.Spinbox(tool_crop, from_=0, to=9999999999999, justify='right')
+        tool_crop_top_text = ttk.Label(tool_crop, text="Point 1 x:")
+        tool_crop_top = ttk.Spinbox(tool_crop, from_=0, to=9999999999999, style="TSpinbox")
+        tool_crop_bottom_text = ttk.Label(tool_crop, text="Point 2 x:")
+        tool_crop_bottom = ttk.Spinbox(tool_crop, from_=0, to=9999999999999, style="TSpinbox")
+        tool_crop_top_text2 = ttk.Label(tool_crop, text="Point 1 y:")
+        tool_crop_top2 = ttk.Spinbox(tool_crop, from_=0, to=9999999999999, style="TSpinbox")
+        tool_crop_bottom_text2 = ttk.Label(tool_crop, text="Point 2 y:")
+        tool_crop_bottom2 = ttk.Spinbox(tool_crop, from_=0, to=9999999999999, style="TSpinbox")
 
         tool_crop_top_text.grid(row=0, column=0, padx=5, pady=3)
         tool_crop_top.grid(row=0, column=1, padx=5, pady=3)
@@ -294,11 +297,20 @@ class Window(tk.Frame):
         tool_crop_bottom_text2.grid(row=1, column=2, padx=5, pady=3)
         tool_crop_bottom2.grid(row=1, column=3, padx=5, pady=3)
 
+        tool_crop_top.delete(0, tk.END)
+        tool_crop_top.insert(0, "0")
+        tool_crop_bottom.delete(0, tk.END)
+        tool_crop_bottom.insert(0, "0")
+        tool_crop_top2.delete(0, tk.END)
+        tool_crop_top2.insert(0, "0")
+        tool_crop_bottom2.delete(0, tk.END)
+        tool_crop_bottom2.insert(0, "0")
+
         # Define widgets for resize
-        tool_resize_vertical_text = tk.Label(tool_resize, text="Vertical Scale:")
-        tool_resize_vertical = tk.Spinbox(tool_resize, from_=0, to=9999999999999, increment=0.1, justify='right')
-        tool_resize_horizontal_text = tk.Label(tool_resize, text="Horizontal Scale:")
-        tool_resize_horizontal = tk.Spinbox(tool_resize, from_=0, to=9999999999999, increment=0.1, justify='right')
+        tool_resize_vertical_text = ttk.Label(tool_resize, text="Vertical Scale:")
+        tool_resize_vertical = ttk.Spinbox(tool_resize, from_=0, to=9999999999999, increment=0.1, style="TSpinbox")
+        tool_resize_horizontal_text = ttk.Label(tool_resize, text="Horizontal Scale:")
+        tool_resize_horizontal = ttk.Spinbox(tool_resize, from_=0, to=9999999999999, increment=0.1, style="TSpinbox")
 
         tool_resize_vertical_text.grid(row=0, column=0, padx=5, pady=3)
         tool_resize_vertical.grid(row=0, column=1, padx=5, pady=3)
@@ -311,8 +323,8 @@ class Window(tk.Frame):
         tool_resize_horizontal.insert(0, "1.0")
 
         # Other buttons
-        tool_apply_button = tk.Button(tool, text="Apply Change", command=execute_file)
-        tool_apply_save_button = tk.Button(tool, text="Apply and Save", command=execute_file)
+        tool_apply_button = ttk.Button(tool, text="Apply Change", command=execute_file)
+        tool_apply_save_button = ttk.Button(tool, text="Apply and Save", command=execute_file)
         tool_apply_button.grid(row=0, column=3, padx=5, pady=5)
         tool_apply_save_button.grid(row=1, column=3, padx=5, pady=5)
 
@@ -325,6 +337,7 @@ def main():
     root.resizable(False, False)
 
     root.mainloop()
+
 
 
 if __name__ == '__main__':
--- tkinter_styles.py
+++ tkinter_styles.py
@@ -6,3 +6,13 @@ Made by: Maximilian Rose
 Created on 20/6/2020
 IDE: PyCharm
 """
+
+import tkinter
+import tkinter.ttk as ttk
+
+
+root = tkinter.Tk()
+root.withdraw()
+
+# Define a Style object
+style = ttk.Style()
